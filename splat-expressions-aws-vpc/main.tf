098397c4287031da734d7ff73c40f7262b4575a1
Giancarlo Haack Albano <gian418@gmail.com>
2022-09-24 18:21:54 -0300

    exemplos splat expressions

---
+++ splat-expressions-aws-vpc/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "curso-terraform-gianhaack"
+    key    = "aws-vpc-splat-expressions/terraform.tfstate"
+    region = "sa-east-1"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+
+  default_tags {
+    tags = {
+      "owner"      = "gianhaack"
+      "managed-by" = "terraform"
+    }
+  }
+}
